839edbdefd406afb1ad82d48404fa8caae5ea55b
Jack Palauskas <spookyjack123@gmail.com>
2024-04-06 21:10:47 -0500

    Added CPU information

--- motd.sh
+++ motd.sh
@@ -5,11 +5,13 @@ primary_interface=$(ip -br link | grep -v 'lo' | grep -v 'wl' | cut -d' ' -f1 | 
 
 # Define dynamic content
 hostname=$(hostname)
+cpu_info=$(awk -F': ' '/model name/ {model=$2} END {print model}' /proc/cpuinfo)
+cpu_cores=$(grep -c ^processor /proc/cpuinfo)
 load=$(uptime | cut -d ' ' -f 9-)
 uptime=$(awk '{print int($1/86400)" days "int(($1%86400)/3600)" hours "int(($1%3600)/60)" minutes "int($1%60)" seconds"}' /proc/uptime)
 ip_address=$(ip addr show $primary_interface | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-local_date=$(date "+%B %d, %Y") # Date format: "January 5, 1980"
-local_time=$(date "+%I:%M:%S %p") # Time format: "8:32:33 PM"
+local_date=$(date "+%B %d, %Y")
+local_time=$(date "+%I:%M:%S %p")
 
 # ASCII Art and dynamic content
 echo "                           -----  ..                        "
@@ -26,23 +28,23 @@ echo "              +.         .: :.:  ++-   +:   =**.  .*        "
 echo "           :-=   :.      :..:  .=.  ==  *    .=*=-.   ..--= "
 echo "          :+-  .+:==-=              .*: :+    -=  :----=--. "
 echo "          =    ::-++-:                +: :=    :=           "
-echo "         .+. .  :.       .    .:-:    .#  .+.   =-          "
-echo "        -=   -:.               .*:     :+   :=:  :=:        "
-echo "       +:  .-::                -#      .#     .=--:-+*+:    "
+echo -e "         .+. .  :.       .    .:-:    .#  .+.   =-          Hostname: ${hostname}"
+echo -e "        -=   -:.               .*:     :+   :=:  :=:        CPU: ${cpu_info} (${cpu_cores}) @ GHz"
+echo -e "       +:  .-::                -#      .#     .=--:-+*+:    Load: ${load}"
 echo "      +:  :::                 .#        =-        .:.       "
 echo "     -=  .        .        . :+:       .:+=                 "
 echo "    .* :=:   -:-+  =    :==-:.            =-                "
 echo "    .#=.   .+..=+     -*-                  :=               "
 echo "     #    =#---   ..=#-                      +.             "
-echo -e "     .-=+=.      .:#+                         =-         "
-echo -e "        +-   .:.    #-            ::.          :+           Hostname: ${hostname}"
-echo -e "          ----::---=-%          .+.            +:           Load: ${load}"
-echo -e "                   -+*.        .*            -=             Uptime: ${uptime}"
-echo -e "                   #+:      :+          .==                 Ethernet IP: ${ip_address}"
+echo -e "     .-=+=.      .:#+                         =-            "
+echo -e "        +-   .:.    #-            ::.          :+           Uptime: ${uptime}"
+echo -e "          ----::---=-%          .+.            +:           Ethernet IP: ${ip_address}"
+echo "                   -+*.        .*            -=             "
+echo "                   #+:      :+          .==               "
 echo "                   +#      -:       .---:                 "
 echo "                   *.    .:   .-----:                     "
 echo "                  -=      :---:                           "
 echo "                  *    :--.                               "
-echo "                 *: .::                                   "
+echo "                 *: .::                                    "
 echo "                 #.:                                      "
 
