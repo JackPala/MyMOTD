9216db4aac810c88bfcff1e3891a983c75f4ec70
Jack Palauskas <spookyjack123@gmail.com>
2024-04-06 21:38:32 -0500

    Add welcome message
    Added welcome message that pulls users first name, with a fallback to username if first name is not available

--- motd.sh
+++ motd.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# Get the current user's login name
+user=$(whoami)
+
+# Attempt to extract the first name, fall back to the username if not available
+first_name=$(getent passwd "$user" | cut -d ':' -f 5 | cut -d ',' -f 1 | awk '{print $1}')
+if [ -z "$first_name" ]; then
+    first_name=$user
+fi
+
+# Generate a bar that matches the terminal width and center the welcome message
+cols=$(tput cols)
+welcome_message="Welcome Back, $first_name"
+message_length=${#welcome_message}
+spaces=$((($cols - $message_length) / 2)) # Calculate spaces needed for centering
+bar_left=$(printf '='%.0s $(seq 1 $spaces))
+bar_right=$(printf '='%.0s $(seq 1 $(($cols - $spaces - $message_length))))
+echo "$bar_left$welcome_message$bar_right"
+echo ""  # This adds the line of whitespace as requested
+
+
 # Dynamically find the first Ethernet interface
 primary_interface=$(ip -br link | grep -v 'lo' | grep -v 'wl' | cut -d' ' -f1 | head -n1)
 
